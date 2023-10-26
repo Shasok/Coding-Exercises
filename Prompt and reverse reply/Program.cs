64610f8cad07319ecf6025f9dc7e2db6a77350e0
Shasok <48457079+Shasok@users.noreply.github.com>
2023-10-26 10:44:04 +0200

    Add files via upload

---
+++ Prompt and reverse reply/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string gecko = Console.ReadLine();
+        for (int i = gecko.Length - 1; i >= 0; i--)
+        {
+            Console.WriteLine(gecko[i]);
+        }
+    }
+}
