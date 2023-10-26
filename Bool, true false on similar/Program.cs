ad97b01beb8eb91b0336bafb45a32c41d0bd6fb8
Shasok <48457079+Shasok@users.noreply.github.com>
2023-10-26 10:38:42 +0200

    Add files via upload

---
+++ Bool, true false on similar/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Diagnostics.Tracing;
+
+namespace Bool__true_false_on_similar
+{
+    internal class Program
+
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            int j = 0;
+            bool z = false;
+            for (int i = word.Length - 1; i >= 0; i--)
+            {
+                if (word[i] == word[j])
+                {
+                    z = true;
+                }
+                else
+                {
+                    z = false;
+                    break;
+                }
+                j++;
+            }
+            if (z)
+            {
+                Console.WriteLine("True");
+            }
+            else
+            {
+                Console.WriteLine("False");
+            }
+        }
+    }
+}
